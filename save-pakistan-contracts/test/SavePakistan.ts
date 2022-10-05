f7333ad4cd64f1fcfff5a2f20ba726ff9c5364ff
Carlo Miguel Dy <carlomigueldy@gmail.com>
2022-10-05 12:15:21 +0800

    Initialize a test suite for SavePakistan ERC1155

--- save-pakistan-contracts/test/SavePakistan.ts
+++ save-pakistan-contracts/test/SavePakistan.ts
@@ -1,3 +1,23 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { ethers } from "hardhat";
+import { SavePakistan } from "../typechain-types";
+
 describe("SavePakistan", () => {
-  // 
-})
+  let savePakistan: SavePakistan;
+  let deployer: SignerWithAddress;
+  let user1: SignerWithAddress;
+  let user2: SignerWithAddress;
+  let signers: SignerWithAddress[];
+
+  before(async () => {
+    [deployer, user1, user2, ...signers] = await ethers.getSigners();
+
+    const SavePakistan = await ethers.getContractFactory("SavePakistan");
+    savePakistan = <SavePakistan>await SavePakistan.deploy();
+    savePakistan.deployed();
+  });
+
+  it("should mint", async () => {
+    // 
+  });
+});
