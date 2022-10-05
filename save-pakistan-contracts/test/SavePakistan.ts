8374c1a5e18e77c13a651bf762f25b2d11272f85
Carlo Miguel Dy <carlomigueldy@gmail.com>
2022-10-05 13:14:23 +0800

    Add test case for calling mintByPayingEth

--- save-pakistan-contracts/test/SavePakistan.ts
+++ save-pakistan-contracts/test/SavePakistan.ts
@@ -1,8 +1,20 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { BigNumber, utils } from "ethers";
 import { ethers } from "hardhat";
 import { SavePakistan } from "../typechain-types";
 
+enum TokenVariant {
+  RationBag = "0",
+  TemporaryShelter = "1",
+  HygieneKit = "2",
+  PortableToilets = "3",
+  Water = "4",
+  WaterWheel = "5",
+}
+
 describe("SavePakistan", () => {
+  const provider = ethers.getDefaultProvider();
   let savePakistan: SavePakistan;
   let deployer: SignerWithAddress;
   let user1: SignerWithAddress;
@@ -15,9 +27,32 @@ describe("SavePakistan", () => {
     const SavePakistan = await ethers.getContractFactory("SavePakistan");
     savePakistan = <SavePakistan>await SavePakistan.deploy();
     savePakistan.deployed();
+
+    const etherBalance = await provider.getBalance(savePakistan.address);
+    console.log("savePakistan contract initial etherBalance", utils.formatEther(etherBalance));
   });
 
-  it("should mint", async () => {
-    // 
+  it("should mint when caller is user1", async () => {
+    const contract = savePakistan.connect(user1);
+
+    const etherMintRate = await contract.getTokenVariantEtherMintRate(
+      BigNumber.from(TokenVariant.RationBag)
+    );
+    console.log("etherMintRate", utils.formatEther(etherMintRate));
+
+    const tx = await contract.mintByPayingEth(
+      BigNumber.from(TokenVariant.RationBag),
+      BigNumber.from("1"),
+      {
+        value: etherMintRate,
+      }
+    );
+    expect(tx.value).to.be.eq(etherMintRate);
+    await tx.wait();
+
+    const etherBalance = await provider.getBalance(savePakistan.address);
+    console.log("etherBalance", utils.formatEther(etherBalance));
+
+    expect(etherBalance).to.be.eq(utils.parseEther("1"));
   });
 });
