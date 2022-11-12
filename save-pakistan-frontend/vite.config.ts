8844d65fb48b2f0ad3b418b05b8e0c80a16e7041
Max Schnaider <maxschnaider@pm.me>
2022-11-12 18:30:15 +0200

    vite fixes: node globals, process.env, polyfill plugin

--- save-pakistan-frontend/vite.config.ts
+++ save-pakistan-frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 export default defineConfig({
   plugins: [vue()],
@@ -8,5 +9,20 @@ export default defineConfig({
     alias: {
       '@': resolve(__dirname, './src'),
     },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+      ],
+    },
+  },
+  define: {
+    'process.env': process.env,
   },
 })
