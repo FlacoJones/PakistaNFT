20c205478348ae7411ec0a9cdcc516c0af295b7e
Max Schnaider <maxschnaider@pm.me>
2022-11-12 17:11:02 +0200

    hero section replace link

--- save-pakistan-frontend/src/views/MainView/components/HeroSection.vue
+++ save-pakistan-frontend/src/views/MainView/components/HeroSection.vue
@@ -2,13 +2,13 @@
   <div
     class="flex-1 min-h-full w-full shadow-cyan-700 shadow-2xl bg-[url('/img/hero-bg.jpg')] bg-cover bg-center bg-no-repeat bg-cyan-800 bg-blend-overlay"
   >
-    <div class="flex flex-col w-full h-full space-y-8 justify-between items-start p-10">
+    <div class="flex flex-col w-full h-full space-y-8 justify-between items-start p-6 lg:p-10">
       <div class="flex w-full justify-between">
-        <a href="https://tayaba.org/" target="_blank" class="hover:scale-105 duration-200">
+        <a href="https://savepakistan.org/" target="_blank" class="hover:scale-105 duration-200">
           <img src="/img/save-pakistan.svg" class="h-20" />
         </a>
         <a
-          href="https://tayaba.org/"
+          href="https://savepakistan.org/"
           target="_blank"
           class="hover:text-green-300 text-white text-xl"
           >About Us
