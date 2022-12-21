431befef6fa0015f7d5fecf10b48944d32b8923c
Max Schnaider <maxschnaider@pm.me>
2022-12-21 02:26:24 +0200

    mutation composables improvements

--- save-pakistan-frontend/src/composables/mutations/useApprove.ts
+++ save-pakistan-frontend/src/composables/mutations/useApprove.ts
@@ -5,24 +5,33 @@ import { Erc20Util } from '@/utils'
 import { BigNumber } from 'ethers'
 
 interface IUseApprove {
+  onSuccess?: () => void
+}
+
+interface IApprove {
   spender: string
   value: BigNumber | undefined
   token: Token
 }
 
-export const useApprove = () => {
+export const useApprove = (options?: IUseApprove) => {
   const mutation = useMutation(
-    async ({ token, spender, value }: IUseApprove) => {
+    async ({ token, spender, value }: IApprove) => {
       const signer = await fetchSigner()
       if (!signer || !token.address || !value) {
         return
       }
+
       const tx = await Erc20Util.Approve(spender, value, token.address, signer)
+      const receipt = await tx.wait()
       return tx
     },
     {
       onError: (error) => console.error(`useApprove | ${error}`),
-      onSuccess: (data) => console.log(`useApprove | onSuccess | ${data}`),
+      onSuccess: (data) => {
+        console.log(`useApprove | onSuccess | ${data}`)
+        options?.onSuccess?.()
+      },
     }
   )
 
