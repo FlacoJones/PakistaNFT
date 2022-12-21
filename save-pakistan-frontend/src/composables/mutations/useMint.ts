7116292b2d67b8a152681b231909937a66f3e20d
Max Schnaider <maxschnaider@pm.me>
2022-12-21 02:26:24 +0200

    mutation composables improvements

--- save-pakistan-frontend/src/composables/mutations/useMint.ts
+++ save-pakistan-frontend/src/composables/mutations/useMint.ts
@@ -3,12 +3,16 @@ import { SPVariant, Token } from '@/types'
 import { SavePakistanUtil } from '@/utils'
 import { fetchSigner } from '@wagmi/core'
 
+interface IUseMint {
+  onSuccess?: () => void
+}
+
 interface IMint {
   variant: SPVariant
   quantity: number
 }
 
-export const useMintWithEth = () => {
+export const useMintWithEth = (options?: IUseMint) => {
   const mutation = useMutation(
     async ({ variant, quantity }: IMint) => {
       const signer = await fetchSigner()
@@ -17,11 +21,17 @@ export const useMintWithEth = () => {
       }
 
       const tx = await SavePakistanUtil.MintWithEth(variant, quantity, signer)
+      // const receipt = await tx.wait()
       return tx
     },
     {
-      onError: (error) => console.error(`useMintWithEth | ${error}`),
-      onSuccess: (data) => console.log(`useMintWithEth | onSuccess | ${data}`),
+      onError: (error) => {
+        console.error(`useMintWithEth | ${error}`)
+      },
+      onSuccess: (data) => {
+        console.log(`useMintWithEth | onSuccess | ${data}`)
+        options?.onSuccess?.()
+      },
     }
   )
 
@@ -32,7 +42,7 @@ interface IMintWithToken extends IMint {
   token: Token
 }
 
-export const useMintWithToken = () => {
+export const useMintWithToken = (options?: IUseMint) => {
   const mutation = useMutation(
     async ({ token, variant, quantity }: IMintWithToken) => {
       const signer = await fetchSigner()
@@ -41,11 +51,17 @@ export const useMintWithToken = () => {
       }
 
       const tx = await SavePakistanUtil.MintWithToken(variant, token, quantity, signer)
+      // const receipt = await tx.wait()
       return tx
     },
     {
-      onError: (error) => console.error(`useMintWithToken | ${error}`),
-      onSuccess: (data) => console.log(`useMintWithToken | onSuccess | ${data}`),
+      onError: (error) => {
+        console.error(`useMintWithToken | ${error}`)
+      },
+      onSuccess: (data) => {
+        console.log(`useMintWithToken | onSuccess | ${data}`)
+        options?.onSuccess?.()
+      },
     }
   )
 
