36ab5a340e20ea48ada356380206dae825fd7322
Carlo Miguel Dy <carlomigueldy@gmail.com>
2022-12-24 06:59:36 +0800

    Add upgrade script

---
+++ save-pakistan-contracts/scripts/upgrade.ts
@@ -0,0 +1,24 @@
+import { ethers, upgrades } from "hardhat";
+import { SavePakistan } from "../typechain-types";
+import { getAddress } from "ethers/lib/utils";
+
+async function main() {
+  // ERC1155
+  const SavePakistan = await ethers.getContractFactory("SavePakistan");
+  const savePakistan = <SavePakistan>(
+    await upgrades.upgradeProxy(
+      getAddress("0xe73b33d6a88a703065e0c22e042b18fd3c4eedc5"),
+      SavePakistan
+    )
+  );
+  await savePakistan.deployed();
+
+  console.log("SavePakistan contract upgraded", savePakistan.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
